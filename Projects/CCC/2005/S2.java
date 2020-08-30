e7c46b4bb8cac529dc3efa96bb8df82d65fdcb51
FranklinGao123 <51179937+FranklinGao123@users.noreply.github.com>
2020-08-30 15:36:53 -0400

    Finished 2005 S2

---
+++ Projects/CCC/2005/S2.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java3;
+import java.util.*;
+import java.io.*;
+/**
+ *
+ * @author frank
+ */
+public class MouseMove {
+    public static class FastReader {
+
+        BufferedReader br;
+        StringTokenizer st;
+        
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+        
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+        
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+        
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+        
+        float nextFloat() {
+            return Float.parseFloat(next());
+        }
+        
+        String nextLine() {
+            String str = null;
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        FastReader s = new FastReader();
+        int c = s.nextInt(), r = s.nextInt();
+        int x = 0, y = 0;
+        while(true){
+            int a = s.nextInt(), b = s.nextInt();
+            if(a==0 && b==0)
+                return;
+            x+=a;
+            y+=b;
+            if(x>c)
+                x=c;
+            if(x<0)
+                x=0;
+            if(y>r)
+                y=r;
+            if(y<0)
+                y=0;
+            System.out.println(x +" "+ y);
+        }
+    }
+}
